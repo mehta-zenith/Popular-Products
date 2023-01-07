672b6d94d11f4d6944808e5085e0720d8df1cd9c
Zenith <mehtazenith7173@gmail.com>
2023-01-07 20:34:14 +0530

    created structure of components

---
+++ src/components/SampleComponent.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// class ProductList extends React.Component {
+//     render() {
+//         <h1>Hello from ProductList</h1>
+//     }
+// }
+
+import Product from "./Product";
+
+// export default ProductList;
+
+function ProductList() {
+    return ( <
+        h1 > hello from component < /h1>
+    );
+}
+export default ProductList;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Product from './components/Product';
+
+import ProductList from './components/SampleComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render( <
+    ProductList / >
 );
 
 // If you want to start measuring performance in your app, pass a function
