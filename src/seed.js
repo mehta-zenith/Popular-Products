4c6f452b99589547998261dc43d4c278d24bb623
Zenith <mehtazenith7173@gmail.com>
2023-01-08 16:13:08 +0530

    modified ProductList component

--- src/seed.js
+++ src/seed.js
@@ -1,3 +1,7 @@
+// seeding is a process of inserting dummy data
+// data aayega parent component ke pass usko child component ko dena hai 
+// passing this data is done through attributes (par isse attributes nhi bolte isse khete hai passing data through props (properties) ku props bolte)
+// jsx = javascript expressions isshe hum {} mai likhte ha
 const Seed = (function() {
     function generateVoteCount() {
       return Math.floor((Math.random() * 50) + 15);
@@ -6,7 +10,7 @@ const Seed = (function() {
     const products = [
       {
         id: 1,
-        title: 'Study Link',
+        title: 'Zenith',
         description: 'A short description about Product One which will describe what the product does!',
         url: '#',
         votes: generateVoteCount(),
